50513b528b6529cc0a99981f8d524a6bd9a9489e
Baaron1 <85914139+Baaron1@users.noreply.github.com>
2021-06-14 22:32:16 -0700

    Added some credits

--- credits.py
+++ credits.py
@@ -1,1 +1,14 @@
+import time
 
+
+print("CREDITS!")
+time.sleep(2)
+print("We hope you enjoyed the game, this is the first game we have made.")
+time.sleep(2)
+print("Anyway here comes the credits   ")
+time.sleep(3)
+print("TextTale:")
+time.sleep(2)
+print("Original idea by: TrazNarf")
+time.sleep(2)
+print("...")

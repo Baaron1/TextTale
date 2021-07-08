c0ba813dc27f2cbbfcd065bfd670f24899da103d
Baaron1 <85914139+Baaron1@users.noreply.github.com>
2021-07-08 08:54:56 -0700

    Update 0.4

--- Start Game/Start Game/Form1.Designer.cs
+++ Start Game/Start Game/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Start_Game
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -171,6 +172,7 @@ namespace Start_Game
             this.Controls.Add(this.button3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "TextTale";
